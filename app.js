eb89967a9dc2ea41e0fc14d0995784c5ac3e6e2c
Eva-547 <evangelin2464@gmail.com>
2025-08-22 10:29:17 +0530

    Fix: redirect root route to /listings

--- app.js
+++ app.js
@@ -67,11 +67,6 @@ const sessionOptions = {
     },
 };
 
-
-// app.get("/", (req,res) => {
-//    res.send("Hi, i'm root.");
-// });
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -87,6 +82,11 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+
+app.get("/", (req,res) => {
+   res.redirect("/listings");
 });
 
 //listing
