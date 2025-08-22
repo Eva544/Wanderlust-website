4a24f2fa76b63789b33dd607837f4c77f91657ae
Eva-547 <evangelin2464@gmail.com>
2025-08-22 11:10:05 +0530

    Add password validation.

--- models/user.js
+++ models/user.js
@@ -11,11 +11,17 @@ const userSchema = new Schema({
 
 userSchema.plugin(passportLocalMongoose, {
     passwordValidator: function(password, cb) {
-        if (password.length < 8) {
-            return cb("Password must be at least 8 characters long");
-        }
-        return cb();
+    if (password.length < 8) {
+        return cb("Password must be at least 8 characters long");
     }
+    if (!/[A-Z]/.test(password)) {
+        return cb("Password must contain at least one uppercase letter");
+    }
+    if (!/[0-9]/.test(password)) {
+        return cb("Password must contain at least one number");
+    }
+    return cb();
+}
 });
 
 module.exports = mongoose.model("User", userSchema);
